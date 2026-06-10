7a31bf830db100327a36f96b039a2440956d489d
Dylan-de-rooze <dylan.derooze@outlook.be>
2026-06-10 09:20:22 +0200

    Change scope and start_url to '/Splitmate/'

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
         background_color: '#1B2B4B',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/splitmate/',
-        start_url: '/splitmate/',
+        scope: '/Splitmate/',
+        start_url: '/Splitmate/',
         icons: [
           { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
